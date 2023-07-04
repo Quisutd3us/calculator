c789dc0f8fb17f1588922dc141dff7a5795c76e0
= <=>
2023-07-04 11:55:08 -0500

    Clean proyect structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
 import './App.css';
+import freeCodeCampLogo from '../src/images/freecodecamp-logo.png'
+
+// import components
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <div className={'logo-container'}>
+                <img
+                    src={freeCodeCampLogo}
+                    className={'logo-img'}
+                    alt={'Logo FreeCodeCamp'}
+                />
+            </div>
+            <div className={'calculator-container'}>
 
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default App;
